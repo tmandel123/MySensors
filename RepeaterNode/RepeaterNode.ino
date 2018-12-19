90d396e830bc9088d88b2a892edc235de1b32d99
tmandel <tmandel@gmx.de>
2018-12-19 12:46:55 +0100

    1.2-003

--- RepeaterNode/RepeaterNode.ino
+++ RepeaterNode/RepeaterNode.ino
@@ -1,19 +1,32 @@
-// #define MY_DEBUG
+#define MY_DEBUG								//nur mit Debug aktiviert können Sends im Abstand von 50ms weitergeleitet werden. Sonst gibt es zu viele NACKs
 
 // Enable and select radio type attached
-#define MY_RADIO_NRF24
+#define MY_RADIO_RF24
 #define MY_RF24_CHANNEL 96
-#define MY_NODE_ID 50
-#define MY_RF24_PA_LEVEL RF24_PA_MAX
-// #define MY_RF24_PA_LEVEL RF24_PA_HIGH
-#define MY_PARENT_NODE_ID 0
+#define MY_NODE_ID 51
+#define MY_PARENT_NODE_ID 100
+#define MY_PARENT_NODE_IS_STATIC
 
+// #define MY_RF24_PA_LEVEL RF24_PA_MAX
+// #define MY_RF24_PA_LEVEL RF24_PA_HIGH
+#define MY_RF24_PA_LEVEL RF24_PA_LOW //läuft ohne Fehler, aber keine große Reichweite
+
+/*
+RF24_PA_MIN = -18dBm 
+RF24_PA_LOW = -12dBm 
+RF24_PA_HIGH = -6dBm 
+RF24_PA_MAX = 0dBm
+*/
 #define MY_TRANSPORT_WAIT_READY_MS (5000ul)
+
+
+#define SKETCH_VER            			"1.2-003"        			// Sketch version
+#define SKETCH_NAME           			"Repeater Node"   		// Optional child sensor name
 
 // Enabled repeater feature for this node
 #define MY_REPEATER_FEATURE
-#define HEARTBEAT_INTERVAL        	60000        //später alle 5 Minuten, zum Test alle 30 Sekunden
-#define CHILD_ID_TEXT				0
+#define HEARTBEAT_INTERVAL        		60000        //später alle 5 Minuten, zum Test alle 30 Sekunden
+#define CHILD_ID_TEXT					0
 
 
 #include <MySensors.h>
@@ -32,7 +45,7 @@ void presentation()
 {
 	present(CHILD_ID_TEXT, S_INFO, "Info Child");
 	//Send the sensor node sketch version information to the gateway
-	sendSketchInfo("Repeater Node", "1.2-001");
+	sendSketchInfo(SKETCH_NAME, SKETCH_VER); 
 	
 }
 
@@ -44,7 +57,7 @@ void loop()
 		// sendHeartbeat();  
 		send(hwTime.set(currentTime),true);
 		lastHeartBeat = currentTime;
-		Serial.println("HEARTBEAT_INTERVAL erreicht");
+		// Serial.println("HEARTBEAT_INTERVAL erreicht");
 
 	}
 }
