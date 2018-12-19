ae4eb614ac5b1296e2380a38cb992317b3d0cc8e
tmandel <tmandel@gmx.de>
2018-12-19 12:44:20 +0100

    1.0-004

--- nRF24Scanner_LED_Version_V1.0/nRF24Scanner_LED_Version_V1.0.ino
+++ nRF24Scanner_LED_Version_V1.0/nRF24Scanner_LED_Version_V1.0.ino
@@ -1,27 +1,34 @@
 // #define MY_DEBUG
 
 // Enable and select radio type attached
-#define MY_RADIO_NRF24
+#define MY_RADIO_RF24
 #define MY_NODE_ID 210
 #define MY_PARENT_NODE_ID 50
+// #define MY_PARENT_NODE_ID 0
+#define MY_PARENT_NODE_IS_STATIC
 
-// #define MY_RF24_CHANNEL 96									// Nach Testphase deaktivieren, damit Kanal 76 aktiv wird
+
+#define MY_RF24_CHANNEL 96									// Nach Testphase deaktivieren, damit Kanal 76 aktiv wird
 
 #define NODE_TXT "Ack Test Node"
-#define MY_INDICATION_HANDLER
+// #define MY_INDICATION_HANDLER
 #define MY_RF24_PA_LEVEL RF24_PA_LOW
-#define MY_TRANSPORT_WAIT_READY_MS (1000ul)
+#define MY_TRANSPORT_WAIT_READY_MS (5000ul)
+
+
+// #define MY_DEBUG_VERBOSE_RF24								//Testen, welche zusätzlichen Infos angezeigt werden
+
 
 #define SER_DEBUG
 
 #include <MySensors.h>
 
-#define SN "Ack-Tester"
-#define SV "1.0-001"
+#define SN 								"Ack-Tester w pRSSI"
+#define SV 								"1.0-004"
 
-#define LED_PIN 						6											// Arduino pin attached to MOSFET Gate pin
+#define LED_PIN 						6						// Arduino pin attached to MOSFET Gate pin
 #define CHILD_ID_TEXT					0
-#define SEND_WAIT						10
+#define SEND_WAIT						40						// 20ms reicht nicht aus, dann kommt immer NACK, bei 22ms läuft es (mit Repeater sollten es 50ms sein)							
 #define REQUEST_ACK						true
 
 #ifdef SER_DEBUG
@@ -66,19 +73,13 @@ void setup()
 {
 	Serial.println("Setup...");
 	// Pull the gateway's current dim level - restore light level upon sendor node power-up
-	request( 0, V_DIMMER );
 	//wdt_reset();
 	//wdt_disable();
-	
-	LED_Blink(2,2);//Anzahl, Geschwindigkeit
-	LED_Blink(10,1);//Anzahl, Geschwindigkeit
 }
 
 void presentation()
 {
-	// Register the LED Dimmable Light with the gateway
 	present(CHILD_ID_TEXT, S_INFO, NODE_TXT);
-	// present( 0, S_DIMMER, NODE_TXT );
 	sendSketchInfo(SN, SV);
 	LED_Blink(1,2);//Anzahl, Geschwindigkeit
 }
@@ -89,16 +90,20 @@ void presentation()
 void loop()
 {
 	unsigned long currentTime = millis();
+	int16_t pRSSI=0;
 	
 	TransportUplink = send(hwTime.set(currentTime), REQUEST_ACK);
-	// DEBUG_PRINT("Uplink Check: ");
-	// DEBUG_PRINTLN(TransportUplink);	
 	wait(SEND_WAIT);
-	wait(100);
 
+	//ToDo:
+	pRSSI=RF24_getSendingRSSI();
+	Serial.print(F("pRSSI: "));
+	Serial.println(pRSSI);
+	
+	
 	if (TransportUplink == false)
 	{
-		Serial.println(F("transportCheckUplink = false"));
+		Serial.println(F("No Ack"));
 		LED_Blink(5,1);//Anzahl, Geschwindigkeit();
 		// LED_Blink(3,2);//Anzahl, Geschwindigkeit();
 	}
@@ -107,27 +112,27 @@ void loop()
 
  
 // ....
-void indication(indication_t ind)
-{
-  switch(ind)
-  {
-    case INDICATION_TX: 
+// void indication(indication_t ind)
+// {
+  // switch(ind)
+  // {
+    // case INDICATION_TX: 
 		// Serial.println(F("TX"));
 		// LED_Blink(1,1);//Anzahl, Geschwindigkeit
-		break;
-    case INDICATION_ERR_FIND_PARENT:          	Serial.println(F("ERR_FIND_PARENT")); break;
-    case INDICATION_GOT_PARENT:          		Serial.println(F("GOT_PARENT")); break;
-    case INDICATION_ERR_HW_INIT: 				Serial.println(F("ERR_HW_INIT"));
-		LED_Blink(10,1);//Anzahl, Geschwindigkeit
-		break;
-    case INDICATION_ERR_TX:
-		Serial.println(F("ERR_TX"));
-		LED_Blink(1,2);//Anzahl, Geschwindigkeit
-		break;
-    case INDICATION_ERR_CHECK_UPLINK:      		Serial.println(F("ERR_CHECK_UPLINK")); break;
+		// break;
+    // case INDICATION_ERR_FIND_PARENT:          	Serial.println(F("ERR_FIND_PARENT")); break;
+    // case INDICATION_GOT_PARENT:          		Serial.println(F("GOT_PARENT")); break;
+    // case INDICATION_ERR_HW_INIT: 				Serial.println(F("ERR_HW_INIT"));
+		// LED_Blink(10,1);//Anzahl, Geschwindigkeit
+		// break;
+    // case INDICATION_ERR_TX:
+		// Serial.println(F("ERR_TX"));
+		// LED_Blink(1,2);//Anzahl, Geschwindigkeit
+		// break;
+    // case INDICATION_ERR_CHECK_UPLINK:      		Serial.println(F("ERR_CHECK_UPLINK")); break;
 
-  }
-}
+  // }
+// }
 
 void LED_Blink(int Anzahl, int Dauer)
 {
@@ -144,8 +149,7 @@ void LED_Blink(int Anzahl, int Dauer)
 		BlinkTime=200;
 		OffTime=100;
 	}
-	// Serial.print("BlinkTime: ");
-	// Serial.println(BlinkTime);
+
 
 	for (int i=0; i<Anzahl; i++){
 		digitalWrite( LED_PIN, HIGH );
@@ -155,12 +159,4 @@ void LED_Blink(int Anzahl, int Dauer)
 	}
 }
 
-// void receive(const MyMessage &message)
-// {
-	// Serial.println("new message data");
-	// Serial.print("type: ");
-	// Serial.println(message.type);
-	// Serial.print("data: ");
-	// Serial.println(message.data);
-// }
 
