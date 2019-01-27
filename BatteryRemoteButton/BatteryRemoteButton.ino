122593a5dbc17115203ea2c8be7d7523c84a2b62
tmandel <tmandel@gmx.de>
2019-01-27 10:11:17 +0100

    1.0-004

--- BatteryRemoteButton/BatteryRemoteButton.ino
+++ BatteryRemoteButton/BatteryRemoteButton.ino
@@ -1,30 +1,33 @@
 //ToDo: 2 Knöpfe (später für An und Aus?)
 // LED blickt, wenn ACK vom Gateway kommt, sonst wird erneut gesendet. nach 3 Sendeversuchen blinkt die LED 3 Mal
 
-
 // Enable debug prints to serial monitor
-// #define MY_DEBUG
+#define MY_DEBUG
 #define SER_DEBUG
 
 #define MY_RF24_CHANNEL 96									// Für Testphase deaktivieren, damit Kanal 76 aktiv wird (Prod=96 Test=76)
 #define MY_RADIO_RF24
 //#define MY_SMART_SLEEP_WAIT_DURATION (1000ul)
 #define MY_NODE_ID 130
-#define MY_PARENT_NODE_ID 0
-#define MY_PARENT_NODE_IS_STATIC
-// #define MY_SPLASH_SCREEN_DISABLED
-#define MY_TRANSPORT_WAIT_READY_MS (0ul)
-// #define MY_TRANSPORT_WAIT_READY_MS (5000ul)
+// #define MY_PARENT_NODE_ID 0
+// #define MY_PARENT_NODE_IS_STATIC
+#define MY_SPLASH_SCREEN_DISABLED
+#define MY_TRANSPORT_WAIT_READY_MS (5000ul)
 
+#define MY_WITH_LEDS_BLINKING_INVERSE
+#define MY_DEFAULT_TX_LED_PIN (8)
+#define MY_DEFAULT_LED_BLINK_PERIOD 10
+
+// #define MY_PASSIVE_NODE
 
 #include <MySensors.h>
-#include <VoltageReference.h>	// https://github.com/rlogiacco/VoltageReference Version 1.2.2
-
+#include <VoltageReference.h>			// https://github.com/rlogiacco/VoltageReference Version 1.2.2
+#include <OneButton.h>					// http://www.mathertel.de/Arduino/OneButtonLibrary.aspx Version 1.3
 
 // #define BATTERY_SENSE_PIN A0
 
 // Node and sketch information
-#define SKETCH_VER            			"1.0-001"        			// Sketch version
+#define SKETCH_VER            			"1.0-004"        			// Sketch version
 #define SKETCH_NAME           			"BatteryRemoteButton"   		// Optional child sensor name
 #define LED_PIN 						8							// Arduino pin attached to MOSFET Gate pin
 #define RECEIVER_NODE					224
@@ -34,22 +37,20 @@
 																	//Batterie ohne Spannungsteiler direkt an A0 anschließen und analogReference(DEFAULT);(VREF bringt hier immer den gleichen Wert)
 #define CHILD_ID_BAT_VREF       		1         					//ID für Batterie Werte Intern per VRef --> Bei Betrieb mit 2-3 Batterien an Arduino mit abgelöteten Spannungsregler
 
-#define PRIMARY_CHILD_ID 				3
-#define SECONDARY_CHILD_ID 				4
+#define BUTTON_CHILD	 				2
 
 #define SEND_WAIT						40							// 20ms reicht nicht aus, dann kommt immer NACK, bei 22ms läuft es (mit Repeater sollten es 50ms sein)							
 #define REQUEST_ACK						true
 
 
-#define PRIMARY_BUTTON_PIN 				2  							// Arduino Digital I/O pin for button/reed switch
-#define SECONDARY_BUTTON_PIN 			3							// Arduino Digital I/O pin for button/reed switch
+#define PRIMARY_BUTTON_PIN 				2
+#define SECONDARY_BUTTON_PIN 			3
 
 #define	BAT_VREF_MAX_VOLTATE			3150
 #define	BAT_VREF_MIN_VOLTATE			1800
 // #define BAT_MESSURED					3510
 // #define BAT_VREF_CORRECTION_VALUE		(float)BAT_VREF_MAX_VOLTATE/(float)BAT_MESSURED
 // #define BAT_VREF_CORRECTION_VALUE		1
-
 
 #ifdef SER_DEBUG
 #define DEBUG_SERIAL(x) Serial.begin(x)
@@ -63,23 +64,27 @@
 
 VoltageReference vRef;
 
+OneButton button1(PRIMARY_BUTTON_PIN,1,true); //Pin, 1=button connected to Ground, true = INPUT_PULLUP
+// OneButton button2(SECONDARY_BUTTON_PIN);
 
 MyMessage BatvRefValue			(CHILD_ID_BAT_VREF,		V_VOLTAGE);
-MyMessage msg					(PRIMARY_CHILD_ID, 		V_TRIPPED);
-MyMessage msg2					(SECONDARY_CHILD_ID, 	V_TRIPPED);
+MyMessage ButtonMsg				(BUTTON_CHILD, 			V_VAR1);
+// MyMessage ButtonVarMsg			(BUTTON_CHILD, 			V_VAR2);
 
-bool TransportUplink = true;
-static uint8_t counter=0;
+// bool TransportUplink = true;
+// static uint8_t counter=0;
+int8_t wakeupReason = 0;
+uint32_t currentTime;
+uint32_t LastButtonUseTime;
 
 void preHwInit() 
 {
 	DEBUG_SERIAL(MY_BAUD_RATE);
 	DEBUG_PRINTLN("preHwInit...");
 
-	pinMode(LED_PIN, OUTPUT);   // sets the pin as output
+	// pinMode(LED_PIN, OUTPUT);   // sets the pin as output
 	// digitalWrite( LED_PIN, HIGH );
-	LED_Blink(1,1);//Anzahl, Geschwindigkeit
-
+	// LED_Blink(1,1);//Anzahl, Geschwindigkeit
 }
 
 void before()
@@ -89,64 +94,85 @@ void before()
 	DEBUG_PRINTLN(SKETCH_VER);
 
 	vRef.begin();
-	LED_Blink(1,2);//Anzahl, Geschwindigkeit
+	// LED_Blink(1,2);//Anzahl, Geschwindigkeit
 	
 }
-
-
-
 
 void setup()
 {
 	// Setup the buttons
-	pinMode(PRIMARY_BUTTON_PIN, INPUT_PULLUP);
-	pinMode(SECONDARY_BUTTON_PIN, INPUT_PULLUP);
+	// pinMode(PRIMARY_BUTTON_PIN, INPUT_PULLUP);
+	// pinMode(SECONDARY_BUTTON_PIN, INPUT_PULLUP);
+	button1.attachDoubleClick(doubleclick1);
+	button1.attachClick(click1);
+	button1.attachLongPressStart(longPressStart1);
+	// button1.attachLongPressStop(longPressStop1);
+	// button1.attachDuringLongPress(longPress1);
+	button1.setDebounceTicks(15);
+	button1.setClickTicks(250);
 }
+
+
 
 void presentation()
 {
 	DEBUG_PRINTLN("presentation...");
-	// Send the sketch version information to the gateway and Controller
-	// sendSketchInfo(SKETCH_NAME, SKETCH_VER);
-	sendSketchInfo(SKETCH_NAME, SKETCH_VER " " __TIME__ " " __DATE__);
-	present(PRIMARY_CHILD_ID, S_DOOR);
-	present(SECONDARY_CHILD_ID, S_DOOR);
-	BatteryVRef();
-	LED_Blink(1,1);//Anzahl, Geschwindigkeit
+	char CompileDate[8];
+	getCompileDateTime(__DATE__, CompileDate);
+	DEBUG_PRINTLN(CompileDate);
+	
+	// char SendString[25] = "01234567890123          "; //mehr als 25 Zeichen werden nicht übertragen
+	char SendString[25] = ""; //mehr als 25 Zeichen werden nicht übertragen
+	uint8_t i=0;
+	for (uint8_t j=0;j<(sizeof(SKETCH_VER)-1);j++)
+	{
+		SendString[j]=SKETCH_VER[j];
+		i++;
+	}
+	SendString[i]=' ';
+	i++;
+	for (uint8_t j=0;j<(sizeof(CompileDate));j++)
+	{
+		SendString[i]=CompileDate[j];
+		i++;
+	}
+	SendString[i]=' ';
+	i++;
+
+	for (uint8_t j=0;j<5;j++)
+	{
+		SendString[i]=__TIME__[j];
+		i++;
+	}
+
+	sendSketchInfo(SKETCH_NAME, SendString );
+	present(BUTTON_CHILD, S_CUSTOM);
+	present(CHILD_ID_BAT_VREF, S_MULTIMETER);
 }
 
 // Loop will iterate on changes on the BUTTON_PINs
 void loop()
 {
-	DEBUG_PRINTLN("loop...");
-	uint8_t value;
-	// static uint8_t sentValue=2;
-	static uint8_t sentValue2=2;
-	// static uint8_t counter=0;
-
-	// Short delay to allow buttons to properly settle
-	sleep(5);
-
-	value = digitalRead(SECONDARY_BUTTON_PIN);
-
-	if (value != sentValue2) {
-		// Value has changed from last transmission, send the updated value
-		DEBUG_PRINT(F("Button"));
-		DEBUG_PRINTLN(value);
-		// send(SisternodeMsg.setDestination(MY_SISTER_NODE_ID).setSensor(CHILD_ID_SISTER_TEMP).set(temperature, 1));
-		// https://forum.mysensors.org/topic/8941/mysensors-get-temperature-value-from-another-node-through-the-gateway/16
-		msg2.setDestination(RECEIVER_NODE);
-		TransportUplink = send(msg2.set(value), REQUEST_ACK);
-		if (TransportUplink == false)
-		{
-			DEBUG_PRINTLN(F("No Ack"));
-			LED_Blink(5,1);//Anzahl, Geschwindigkeit();
-		}
-		else
-		{
-			LED_Blink(1,1);
-		}
-		sentValue2 = value;
+	static int counter=0;
+	currentTime = millis();
+	// DEBUG_PRINT("Button Tick ");
+	// DEBUG_PRINTLN(currentTime);
+	
+	button1.tick();
+	
+	if ((currentTime - LastButtonUseTime) > 3000)
+	{
+		DEBUG_PRINTLN("TimeToSleep");
+		// DEBUG_PRINT(currentTime);
+		// DEBUG_PRINT(" ");
+		// DEBUG_PRINTLN(LastButtonUseTime);
+		
+		wakeupReason = sleep(PRIMARY_BUTTON_PIN-2, CHANGE, SECONDARY_BUTTON_PIN-2, CHANGE, 0);
+		button1.tick();
+		currentTime = millis();
+		LastButtonUseTime = currentTime;
+		// DEBUG_PRINT("wakeupReason: "); // 0 oder 1 je nach Button
+		// DEBUG_PRINTLN(wakeupReason);
 		counter++;
 	}
 	
@@ -155,12 +181,68 @@ void loop()
 		counter = 0;
 		BatteryVRef();
 	}
-	
-	
-	// Sleep until something happens with the sensor
-	sleep(PRIMARY_BUTTON_PIN-2, CHANGE, SECONDARY_BUTTON_PIN-2, CHANGE, 0);
 }
 
+
+void click1()
+{
+	int Counter=0;
+	bool sendStatus=false;
+	DEBUG_PRINTLN("click1 d");
+	// ButtonMsg.setDestination(RECEIVER_NODE);
+	while ( !sendStatus and (Counter < 5))
+	{
+		// ButtonMsg.setDestination(RECEIVER_NODE);
+		sendStatus = send(ButtonMsg.set("d"), REQUEST_ACK);
+		DEBUG_PRINT("sendStatus >");
+		DEBUG_PRINT(sendStatus);
+		DEBUG_PRINTLN("<");
+		Counter++;
+	}
+	LastButtonUseTime=currentTime;
+}
+
+void longPressStart1()
+{
+	// int Counter=0;
+	// bool sendStatus=false;
+	DEBUG_PRINTLN("longPressStop1 R");
+	// ButtonMsg.setDestination(RECEIVER_NODE);
+	DEBUG_PRINTLN(send(ButtonMsg.set("R"), REQUEST_ACK));
+	// while ( !sendStatus and (Counter < 5))
+	// {
+		// sendStatus = send(ButtonMsg.set("R"), REQUEST_ACK);
+		// Counter++;
+	// }
+	LastButtonUseTime=currentTime;
+}
+
+void doubleclick1()
+{
+	int Counter=0;
+	bool sendStatus=false;
+	DEBUG_PRINTLN("doubleclick1 D");	
+	// ButtonMsg.setDestination(RECEIVER_NODE);
+	while ( !sendStatus and (Counter < 5))
+	{
+		sendStatus = send(ButtonMsg.set("D"), REQUEST_ACK);
+		Counter++;
+	}
+	LastButtonUseTime=currentTime;
+}
+
+
+void longPressStop1()
+{
+	DEBUG_PRINTLN("longPressStop1");
+	LastButtonUseTime=currentTime;
+}
+
+void longPress1()
+{
+	DEBUG_PRINTLN("longPress1");
+	LastButtonUseTime=currentTime;
+}
 
 void BatteryVRef()
 {
@@ -189,27 +271,35 @@ void BatteryVRef()
 }
 
 
-void LED_Blink(int Anzahl, int Dauer)
-{
+// void LED_Blink(int Anzahl, int Dauer)
+// {
 
-	int OffTime;
-	int BlinkTime;
-	if (Dauer == 1)
-	{
-		BlinkTime=20;
-		OffTime=10;
-	}
-	else
-	{
-		BlinkTime=200;
-		OffTime=200;
-	}
+	// int OffTime;
+	// int BlinkTime;
+	// if (Dauer == 1)
+	// {
+		// BlinkTime=20;
+		// OffTime=10;
+	// }
+	// else
+	// {
+		// BlinkTime=200;
+		// OffTime=200;
+	// }
 
 
-	for (int i=0; i<Anzahl; i++){
-		digitalWrite( LED_PIN, HIGH );
-		wait(BlinkTime);
-		digitalWrite( LED_PIN, LOW );
-		wait(OffTime);
-	}
+	// for (int i=0; i<Anzahl; i++){
+		// digitalWrite( LED_PIN, HIGH );
+		// wait(BlinkTime);
+		// digitalWrite( LED_PIN, LOW );
+		// wait(OffTime);
+	// }
+// }
+
+void getCompileDateTime(char const *date, char *buff) {
+    int month, day, year;
+    static const char month_names[] = "JanFebMarAprMayJunJulAugSepOctNovDec";
+    sscanf(date, "%s %d %d", buff, &day, &year);
+    month = (strstr(month_names, buff)-month_names)/3+1;
+    sprintf(buff, "%d%02d%02d", year, month, day);
 }
