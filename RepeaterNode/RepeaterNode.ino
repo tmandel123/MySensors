4f38f7234b3d7c2e4467d810d536c9aa55ee0874
tmandel <tmandel@gmx.de>
2019-01-27 11:56:11 +0100

    1.2-004

--- RepeaterNode/RepeaterNode.ino
+++ RepeaterNode/RepeaterNode.ino
@@ -1,15 +1,15 @@
-#define MY_DEBUG								//nur mit Debug aktiviert können Sends im Abstand von 50ms weitergeleitet werden. Sonst gibt es zu viele NACKs
+// #define MY_DEBUG								//nur mit Debug aktiviert können Sends im Abstand von 50ms weitergeleitet werden. Sonst gibt es zu viele NACKs
 
 // Enable and select radio type attached
 #define MY_RADIO_RF24
 #define MY_RF24_CHANNEL 96
-#define MY_NODE_ID 51
-#define MY_PARENT_NODE_ID 100
+#define MY_NODE_ID 50
+#define MY_PARENT_NODE_ID 0
 #define MY_PARENT_NODE_IS_STATIC
 
-// #define MY_RF24_PA_LEVEL RF24_PA_MAX
+#define MY_RF24_PA_LEVEL RF24_PA_MAX	//NodeID 50, seit MySensors 2.3.1 scheint auch PA_MAX zu funktionieren (Shielded Modul)
 // #define MY_RF24_PA_LEVEL RF24_PA_HIGH
-#define MY_RF24_PA_LEVEL RF24_PA_LOW //läuft ohne Fehler, aber keine große Reichweite
+// #define MY_RF24_PA_LEVEL RF24_PA_LOW //läuft ohne Fehler, aber keine große Reichweite
 
 /*
 RF24_PA_MIN = -18dBm 
@@ -20,7 +20,7 @@ RF24_PA_MAX = 0dBm
 #define MY_TRANSPORT_WAIT_READY_MS (5000ul)
 
 
-#define SKETCH_VER            			"1.2-003"        			// Sketch version
+#define SKETCH_VER            			"1.2-004"        			// Sketch version
 #define SKETCH_NAME           			"Repeater Node"   		// Optional child sensor name
 
 // Enabled repeater feature for this node
@@ -31,10 +31,32 @@ RF24_PA_MAX = 0dBm
 
 #include <MySensors.h>
 
+#ifdef SER_DEBUG
+#define DEBUG_SERIAL(x) Serial.begin(x)
+#define DEBUG_PRINT(x) Serial.print(x)
+#define DEBUG_PRINTLN(x) Serial.println(x)
+#else
+#define DEBUG_SERIAL(x)
+#define DEBUG_PRINT(x) 
+#define DEBUG_PRINTLN(x) 
+#endif
 
 MyMessage hwTime		(CHILD_ID_TEXT, V_TEXT);
 unsigned long lastHeartBeat = HEARTBEAT_INTERVAL - 8000;
 
+
+void preHwInit() 
+{
+	DEBUG_SERIAL(MY_BAUD_RATE);
+	DEBUG_PRINTLN("preHwInit...");
+}
+
+void before()
+{
+	DEBUG_PRINTLN("before...");
+	DEBUG_PRINT("Version: ");
+	DEBUG_PRINTLN(SKETCH_VER);
+}
 
 void setup()
 {
@@ -43,10 +65,37 @@ void setup()
 
 void presentation()
 {
-	present(CHILD_ID_TEXT, S_INFO, "Info Child");
-	//Send the sensor node sketch version information to the gateway
-	sendSketchInfo(SKETCH_NAME, SKETCH_VER); 
+	DEBUG_PRINTLN("presentation...");
+	char CompileDate[8];
+	getCompileDateTime(__DATE__, CompileDate);
+	DEBUG_PRINTLN(CompileDate);
 	
+	char SendString[25] = ""; //mehr als 25 Zeichen werden nicht übertragen
+	uint8_t i=0;
+	for (uint8_t j=0;j<(sizeof(SKETCH_VER)-1);j++)
+	{
+		SendString[j]=SKETCH_VER[j];
+		i++;
+	}
+	SendString[i]=' ';
+	i++;
+	for (uint8_t j=0;j<(sizeof(CompileDate));j++)
+	{
+		SendString[i]=CompileDate[j];
+		i++;
+	}
+	SendString[i]=' ';
+	i++;
+
+	for (uint8_t j=0;j<5;j++)
+	{
+		SendString[i]=__TIME__[j];
+		i++;
+	}
+
+	sendSketchInfo(SKETCH_NAME, SendString );
+
+	present(CHILD_ID_TEXT, S_INFO, "Info Child");
 }
 
 void loop()
@@ -60,4 +109,12 @@ void loop()
 		// Serial.println("HEARTBEAT_INTERVAL erreicht");
 
 	}
+}
+
+void getCompileDateTime(char const *date, char *buff) {
+    int month, day, year;
+    static const char month_names[] = "JanFebMarAprMayJunJulAugSepOctNovDec";
+    sscanf(date, "%s %d %d", buff, &day, &year);
+    month = (strstr(month_names, buff)-month_names)/3+1;
+    sprintf(buff, "%d%02d%02d", year, month, day);
 }
