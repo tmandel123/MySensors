f590682a704ed63d66880188ce6a94aaea0f4fa0
tmandel <tmandel@gmx.de>
2019-02-06 12:01:10 +0100

    1.5-002

--- EnergyMeterPulseSensor/EnergyMeterPulseSensor.ino
+++ EnergyMeterPulseSensor/EnergyMeterPulseSensor.ino
@@ -1,31 +1,12 @@
 /**
- * The MySensors Arduino library handles the wireless radio link and protocol
- * between your home built sensors/actuators and HA controller of choice.
- * The sensors forms a self healing radio network with optional repeaters. Each
- * repeater and gateway builds a routing tables in EEPROM which keeps track of the
- * network topology allowing messages to be routed to nodes.
- *
- * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
- * Copyright (C) 2013-2015 Sensnology AB
- * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
- *
- * Documentation: http://www.mysensors.org
- * Support Forum: http://forum.mysensors.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- *******************************/
  
-
-//	20181112 Version 1.32		millis() von "uint32_t now" nach "unsigned long currentTime" und wieder zurück
-//	20181112 Version 1.33		Sleep_mode ausgebaut und DEBUG_PRINT eingeführt
-//	20181114 Version 1.34		boolean informGW = false; - > bool informGW = false;
-//	20181114 Version 1.4-003	MY_REPEATER_FEATURE deaktiviert und sendSketchInfo(SKETCH_NAME, SKETCH_VER " " __TIME__ " " __DATE__);
-//
+20181112 Version 1.32		millis() von "uint32_t now" nach "unsigned long currentTime" und wieder zurück
+20181112 Version 1.33		Sleep_mode ausgebaut und DEBUG_PRINT eingeführt
+20181114 Version 1.34		boolean informGW = false; - > bool informGW = false;
+20181114 Version 1.4-003	MY_REPEATER_FEATURE deaktiviert und sendSketchInfo(SKETCH_NAME, SKETCH_VER " " __TIME__ " " __DATE__);
 
 
+*************************************************/
 
 // Enable debug prints
 // #define MY_DEBUG
@@ -47,7 +28,7 @@
 #include <MySensors.h>
 
 #define SKETCH_NAME						"EnergyMeter"			// Optional child sensor name
-#define SKETCH_VER						"1.4-003"				// Sketch version
+#define SKETCH_VER						"1.5-002"				// Sketch version
 
 
 
