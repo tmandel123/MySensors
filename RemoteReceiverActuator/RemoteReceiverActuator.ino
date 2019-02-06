02d78a66dacecddd245518b1b14d26a3460fea27
tmandel <tmandel@gmx.de>
2019-02-06 12:00:19 +0100

    1.0-005

--- RemoteReceiverActuator/RemoteReceiverActuator.ino
+++ RemoteReceiverActuator/RemoteReceiverActuator.ino
@@ -1,75 +1,63 @@
-
-// Enable debug prints to serial monitor
-#define MY_DEBUG
-#define MY_SPECIAL_DEBUG
-
-// #define MY_REPEATER_FEATURE
-
-
-
-
-// Enable and select radio type attached
-#define MY_RADIO_RF24
-#define MY_RF24_SANITY_CHECK
-
-#define MY_NODE_ID 224
-// #define MY_PARENT_NODE_ID 51
-// #define MY_PARENT_NODE_IS_STATIC
-#define MY_RF24_CHANNEL 96									// Für Testphase deaktivieren, damit Kanal 76 aktiv wird (Prod=96 Test=76)
-
-/**
- * @def MY_RF24_PA_LEVEL
- * @brief Default RF24 PA level. Override in sketch if needed.
- *
- * - RF24_PA_MIN = -18dBm
- * - RF24_PA_LOW = -12dBm
- * - RF24_PA_HIGH = -6dBm
- * - RF24_PA_MAX = 0dBm
- */
- 
-#define MY_RF24_PA_LEVEL RF24_PA_LOW
-
+//	###################   Debugging   #####################
+#define MY_DEBUG								//nur mit Debug aktiviert können Sends im Abstand von 50ms weitergeleitet werden. Sonst gibt es zu viele NACKs
+#define SER_DEBUG
+// #define MY_DEBUG_VERBOSE_RF24								//Testen, welche zusätzlichen Infos angezeigt werden
+#define MY_SPLASH_SCREEN_DISABLED
 #define MY_SIGNAL_REPORT_ENABLED
 
+//	###################   Features   #####################
+// #define MY_REPEATER_FEATURE
 
-#define MY_TRANSPORT_WAIT_READY_MS (5000ul)
-// #define MY_INDICATION_HANDLER
+//	###################   LEDs   #####################
+#define MY_WITH_LEDS_BLINKING_INVERSE
+#define MY_DEFAULT_TX_LED_PIN 				(8)
+#define MY_DEFAULT_LED_BLINK_PERIOD 		10
+
+// ###################   Transport   #####################
+/*
+RF24_PA_MIN = -18dBm 
+RF24_PA_LOW = -12dBm 
+RF24_PA_HIGH = -6dBm 
+RF24_PA_MAX = 0dBm
+*/
+#define MY_RF24_PA_LEVEL 					RF24_PA_LOW
+#define MY_RADIO_RF24
+#define MY_RF24_CHANNEL 					96
+// #define MY_TRANSPORT_WAIT_READY_MS 			(5000ul)
+#define MY_RF24_SANITY_CHECK
+
+#define MY_NODE_ID 							224
+#define MY_PARENT_NODE_ID 					0
+#define MY_PARENT_NODE_IS_STATIC
+// #define MY_PASSIVE_NODE
+
+
+#define SKETCH_NAME 						"RemoteReceiverActuator"
+#define SKETCH_VER 							"1.0-005"
+
+#define	SERVO_BIG_STEP						20
+#define	SERVO_SMALL_STEP					5
+// #define LED_PIN 							3      				// Arduino pin attached to MOSFET Gate pin
+// #define FADE_DELAY 						2  					// Delay in ms for each percentage fade up/down (10ms = 1s full-range dim)
+
+
+#define SENDER_REMOTE_NODE					130
+
+#define HEARTBEAT_INTERVAL					30000        //später alle 5 Minuten, zum Test alle 30 Sekunden
 
 
 
 #include <MySensors.h>
 #include <Servo.h>
+#include "C:\_Lokale_Daten_ungesichert\Arduino\MySensors\CommonFunctions.h" //muss nach allen anderen #defines stehen
 
 
-#define SKETCH_NAME 				"RemoteReceiverActuator"
-#define SKETCH_VER 					"1.0-003-2"
+uint32_t lastHeartBeat = HEARTBEAT_INTERVAL - 5000; //das erste Mal sollte nach 5 Sekunden etwas passieren
 
-#define	SERVO_BIG_STEP				20
-#define	SERVO_SMALL_STEP			5
-// #define LED_PIN 					3      				// Arduino pin attached to MOSFET Gate pin
-// #define FADE_DELAY 				2  					// Delay in ms for each percentage fade up/down (10ms = 1s full-range dim)
-
-#define CHILD_ID_LED       			0     				//ID für Child, welches den MOSFET per PWM ansteuert
-#define CHILD_ID_LED_TEXT			"BUTTON_0"
-#define LED_CHILD_0_EEPROM			0
-#define SENDER_REMOTE_NODE			130
-
-#define HEARTBEAT_INTERVAL			300000        //später alle 5 Minuten, zum Test alle 30 Sekunden
-
-#define SER_DEBUG
-#ifdef SER_DEBUG
-#define DEBUG_SERIAL(x) Serial.begin(x)
-#define DEBUG_PRINT(x) Serial.print(x)
-#define DEBUG_PRINTLN(x) Serial.println(x)
-#else
-#define DEBUG_SERIAL(x)
-#define DEBUG_PRINT(x) 
-#define DEBUG_PRINTLN(x) 
-#endif
-
-unsigned long lastHeartBeat = HEARTBEAT_INTERVAL - 5000; //das erste Mal sollte nach 5 Sekunden etwas passieren
-
-MyMessage ButtonMsg					(CHILD_ID_LED, V_VAR1);
+// MyMessage MsgMultiButton					(CHILD_BUTTON , V_TEXT);
+// MyMessage MsgServoState				(CHILD_SERVO_STATE , V_TEXT);
+// MyMessage MsgHwTime					(CHILD_HWTIME, V_TEXT);
+// MyMessage MsgSendingRSSI			(CHILD_TX_POWER_LEVEL, V_TEXT);
 
 Servo myservo;  // create servo object to control a servo
 
@@ -84,21 +72,21 @@ void preHwInit()
 	DEBUG_SERIAL(MY_BAUD_RATE);
 	DEBUG_PRINTLN("preHwInit...");
 	myservo.attach(14);  // attaches the servo on pin 9 to the servo object A0=14
-	ServoReset();
+	myservo.write(50);
+	wait(500);
+	myservo.write(110);
 }
 
-void before() 
+void before()
 {
-	DEBUG_PRINTLN("before: ");
-	DEBUG_PRINT(SKETCH_NAME);
-	DEBUG_PRINT(" ");
-	DEBUG_PRINTLN(SKETCH_VER);
-	DEBUG_PRINT(__TIME__);
-	DEBUG_PRINT(" ");
-	DEBUG_PRINTLN(__DATE__);
+	DEBUG_PRINTLN("before...");
+    // DEBUG_PRINTLN("Started clearing. Please wait...");
+    // for (uint16_t i=0; i<EEPROM_LOCAL_CONFIG_ADDRESS; i++) {
+		// DEBUG_PRINTLN(i);
+        // hwWriteConfig(i,0xFF);
+    // }
+    // DEBUG_PRINTLN("Clearing done.");
 }
-
-
 
 void setup()
 {
@@ -106,54 +94,25 @@ void setup()
 
 }
 
-
-
 void presentation()
 {
 	DEBUG_PRINTLN("presentation...");
-	char CompileDate[8];
-	getCompileDateTime(__DATE__, CompileDate);
-	DEBUG_PRINTLN(CompileDate);
-	
-	// char SendString[25] = "01234567890123          "; //mehr als 25 Zeichen werden nicht übertragen
-	char SendString[25] = ""; //mehr als 25 Zeichen werden nicht übertragen
-	uint8_t i=0;
-	for (uint8_t j=0;j<(sizeof(SKETCH_VER)-1);j++)
-	{
-		SendString[j]=SKETCH_VER[j];
-		i++;
-	}
-	SendString[i]=' ';
-	i++;
-	for (uint8_t j=0;j<(sizeof(CompileDate));j++)
-	{
-		SendString[i]=CompileDate[j];
-		i++;
-	}
-	SendString[i]=' ';
-	i++;
-
-	for (uint8_t j=0;j<5;j++)
-	{
-		SendString[i]=__TIME__[j];
-		i++;
-	}
-
-	sendSketchInfo(SKETCH_NAME, SendString );
-	present(CHILD_ID_LED, S_CUSTOM, CHILD_ID_LED_TEXT);
-
+	mySendSketchInfo();
+	myPresentation();
+	present(CHILD_MULTI_BUTTON,		S_INFO,	CHILD_MULTI_BUTTON_TEXT);
+	present(CHILD_SERVO_STATE,		S_INFO, CHILD_SERVO_STATE_TEXT);
 }
-
 
 void loop()
 {
 	uint32_t currentTime = millis();
 	
-	if (currentTime - lastHeartBeat > (unsigned long)HEARTBEAT_INTERVAL)
+	if (currentTime - lastHeartBeat > (uint32_t)HEARTBEAT_INTERVAL)
 	{
+		myHeartBeatLoop();
 		lastHeartBeat = currentTime;
-		DEBUG_PRINTLN("HeartBeat");
 	}
+	
 	if (ServoPos != LastServoPos)
 	{
 		LastServoPos = ServoPos;
@@ -163,41 +122,21 @@ void loop()
 	
 }
 
-
-
 void receive(const MyMessage &message)
 {
 	
-	// DEBUG_PRINT("sensor: ");
-	// DEBUG_PRINTLN(message.sensor);
 
-	DEBUG_PRINT("mGetPayloadType: ");
-	DEBUG_PRINTLN(mGetPayloadType(message));
-	
-	// DEBUG_PRINT("sender: ");
-	// DEBUG_PRINTLN(message.sender);
-
-	// DEBUG_PRINT("version: ");
-	// DEBUG_PRINTLN(message.version);
-	
-	// DEBUG_PRINT("getByte: ");
-	// uint8_t Temp = message.getByte();
-	// DEBUG_PRINTLN(Temp);
-	
 	
 		
 	// if ((message.sender == SENDER_REMOTE_NODE) && !mGetAck(message))
 	if (!mGetAck(message))
 	{
-		if (message.type == V_VAR1)
+		if ((message.type == V_TEXT) && (message.sensor == CHILD_MULTI_BUTTON))
 		{
-			uint8_t bData = message.getByte();
-			DEBUG_PRINT("getByte: ");
-			DEBUG_PRINTLN(bData);
-			
+
 			String sData = message.getString();
-			DEBUG_PRINT("getString: ");
-			DEBUG_PRINTLN(sData);
+			// DEBUG_PRINT("getString: ");
+			// DEBUG_PRINTLN(sData);
 			
 			if (sData[0] == 'U')
 			{
@@ -222,7 +161,13 @@ void receive(const MyMessage &message)
 			else if (sData[0] == 'R')
 			{
 				ServoReset();
+				// PrintRF24Transport();
 				DEBUG_PRINTLN("R");
+			}
+			else
+			{
+				DEBUG_PRINT("unknown V_TEXT: ");
+				DEBUG_PRINTLN(sData);
 			}
 		}
 	}
@@ -230,8 +175,22 @@ void receive(const MyMessage &message)
 	{
 		DEBUG_PRINTLN("unknown receive");
 		
+		DEBUG_PRINT("sensor: ");
+		DEBUG_PRINTLN(message.sensor);
 
+		DEBUG_PRINT("mGetPayloadType: ");
+		DEBUG_PRINTLN(mGetPayloadType(message));
+		
+		DEBUG_PRINT("sender: ");
+		DEBUG_PRINTLN(message.sender);
 
+		DEBUG_PRINT("version: ");
+		DEBUG_PRINTLN(message.version);
+		
+		DEBUG_PRINT("getByte: ");
+		uint8_t Temp = message.getByte();
+		DEBUG_PRINTLN(Temp);
+	
 	}
 
 }
@@ -240,7 +199,8 @@ void ServoReset()
 {
 	DEBUG_PRINTLN("ServoReset");
 	ServoPos=90;
-	myservo.write(ServoPos);  
+	myservo.write(ServoPos);
+	mySendInt8(MsgServoState, ServoPos);
 }
 
 void ServoDown(uint8_t steps)
@@ -254,7 +214,8 @@ void ServoDown(uint8_t steps)
 	{
 		ServoPos=180;
 	}
-	myservo.write(ServoPos); 
+	myservo.write(ServoPos);
+	mySendInt8(MsgServoState, ServoPos);
 }
 
 void ServoUp(uint8_t steps)
@@ -269,14 +230,6 @@ void ServoUp(uint8_t steps)
 		ServoPos=0;
 	}
 	myservo.write(ServoPos); 
+	mySendInt8(MsgServoState, ServoPos);
 }
-
-void getCompileDateTime(char const *date, char *buff) {
-    int month, day, year;
-    static const char month_names[] = "JanFebMarAprMayJunJulAugSepOctNovDec";
-    sscanf(date, "%s %d %d", buff, &day, &year);
-    month = (strstr(month_names, buff)-month_names)/3+1;
-    sprintf(buff, "%d%02d%02d", year, month, day);
-}
-
 
