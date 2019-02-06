fda3f9a790347220c5265f6b3434e8a865fdb13c
tmandel <tmandel@gmx.de>
2019-02-06 12:01:36 +0100

    2.4.1-017

--- GasWaterMeterA0Sensor/GasWaterMeterA0Sensor.ino
+++ GasWaterMeterA0Sensor/GasWaterMeterA0Sensor.ino
@@ -1,24 +1,4 @@
 /**
- * The MySensors Arduino library handles the wireless radio link and protocol
- * between your home built sensors/actuators and HA controller of choice.
- * The sensors forms a self healing radio network with optional repeaters. Each
- * repeater and gateway builds a routing tables in EEPROM which keeps track of the
- * network topology allowing messages to be routed to nodes.
- *
- * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
- * Copyright (C) 2013-2015 Sensnology AB
- * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
- *
- * Documentation: http://www.mysensors.org
- * Support Forum: http://forum.mysensors.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- *******************************/
- 
-/*
 Sensoren:	Gas		Arduino Raspberry kompatible Linear Hall Magnetic Sensor Module KY-024 	(1,35 EUR)
 					Angeschlossen an 3,3V 
 					kein Magnet in der Nähe: 			AnalogValue=512
@@ -58,7 +38,7 @@ set MYSENSOR_102 value52 338900 				//set a now gas/water meter value
 
 */
 
-#define SKETCH_VER						"2.4.1-016"				// Sketch version
+#define SKETCH_VER						"2.4.1-017"				// Sketch version
 #define MY_RADIO_RF24
 
 // #define MY_DEBUG //muss vor MySensors.h stehen
