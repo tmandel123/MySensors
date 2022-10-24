f57b392d96de1002b9690d17a1da6a30b5e13377
tmandel <tmandel@gmx.de>
2022-10-24 14:28:48 +0200

    1.0-007		2022-05-10	Anpassungen für Mysensors 2.3.2 begonnen

--- EchoNode/EchoNode.ino
+++ EchoNode/EchoNode.ino
@@ -1,3 +1,9 @@
+/*
+1.0-007		2022-05-10	Anpassungen für Mysensors 2.3.2 begonnen
+
+*/
+
+
 /***
 
 Echo Node soll immer dort aufgestellt werden, wo später ein aktiver Aktor oder Sensor betrieben wird.
@@ -21,10 +27,12 @@ Grundsätzliche funktionsweise:
 */
 
 //	###################   Debugging   #####################
-// #define MY_DEBUG
-#define SER_DEBUG
+#define MY_DEBUG												//Output kann im LogParser analysiert werden https://www.mysensors.org/build/parser
+#define SER_DEBUG											// aus CommonFunctions.h für eigenes DEBUG_PRINT
+#define MY_SPECIAL_DEBUG									// für Extended Debug in FHEM 'R': routing info 'V': CPU voltage 'F': CPU frequency 'M': free memory 'E': clear MySensors EEPROM area and reboot
+
 // #define MY_DEBUG_VERBOSE_RF24								//Testen, welche zusätzlichen Infos angezeigt werden
-#define MY_SPLASH_SCREEN_DISABLED
+// #define MY_SPLASH_SCREEN_DISABLED
 // #define MY_SIGNAL_REPORT_ENABLED
 
 //	###################   Features   #####################
@@ -32,10 +40,10 @@ Grundsätzliche funktionsweise:
 
 //	###################   LEDs   #####################
 // #define MY_WITH_LEDS_BLINKING_INVERSE
-#define MY_DEFAULT_RX_LED_PIN				17
-#define MY_DEFAULT_TX_LED_PIN 				18
-#define MY_DEFAULT_ERR_LED_PIN				19
-#define MY_DEFAULT_LED_BLINK_PERIOD 		10
+// #define MY_DEFAULT_RX_LED_PIN				17
+// #define MY_DEFAULT_TX_LED_PIN 				18
+// #define MY_DEFAULT_ERR_LED_PIN				19
+// #define MY_DEFAULT_LED_BLINK_PERIOD 		10
 
 // ###################   Transport   #####################
 /*
@@ -44,67 +52,69 @@ RF24_PA_LOW = -12dBm
 RF24_PA_HIGH = -6dBm 
 RF24_PA_MAX = 0dBm
 */
-#define MY_RF24_PA_LEVEL 					RF24_PA_HIGH
+#define MY_RF24_PA_LEVEL 					RF24_PA_LOW
 #define MY_RADIO_RF24
 #define MY_RF24_CHANNEL 					96
 // #define MY_TRANSPORT_WAIT_READY_MS			(5000ul)
 // #define MY_TRANSPORT_SANITY_CHECK
 
 #define MY_NODE_ID 							225
-#define MY_PARENT_NODE_ID 					0
-#define MY_PARENT_NODE_IS_STATIC
+// #define MY_PARENT_NODE_ID 					50
+// #define MY_PARENT_NODE_IS_STATIC
 // #define MY_PASSIVE_NODE
+
+#define MY_INDICATION_HANDLER									//erlaubt rewrite der Funktion void indication(indication_t ind) 
+
 
 
 // ###################   Node Spezifisch   #####################
-#define SKETCH_VER            				"1.0-006"        			// Sketch version
+#define SKETCH_VER            				"1.0-007"        			// Sketch version
 #define SKETCH_NAME           				"Echo Node"   		// Optional child sensor name
 #define MY_ECHO_NODE											//for some more myPresentation() sendings
 
-// #define LED_PIN 							6						// Arduino pin attached to MOSFET Gate pin
 // #define SEND_WAIT							20						// 20ms reicht nicht aus, dann kommt immer NACK, bei 22ms läuft es (mit Repeater sollten es 50ms sein)							
 // #define REQUEST_ACK							true
 
-#define HEARTBEAT_INTERVAL        			300000        //später alle 5 Minuten, zum Test alle 30 Sekunden
-// #define ECHO_TXFAIL_RESET_TIME     			10000
-#define MAX_ECHO_WAIT	        			1000
-
-
+#define HEARTBEAT_INTERVAL        			30000        		//später alle 5 Minuten, zum Test alle 30 Sekunden
+#define MAX_ECHO_WAIT	        			500					//nach dieser Zeit wird der TxFailCounter hochgezählt
+#define PING_INTERVAL	        			3000
 
 #include <MySensors.h>
 #include "C:\_Lokale_Daten_ungesichert\Arduino\MySensors\CommonFunctions.h" //muss nach allen anderen #defines stehen
 
-bool 		GotEchoResponse = true;
-uint32_t	TxFailCounter = 0;
-uint32_t	TxGoodCounter = 0;
-uint16_t	maxPingTime = 1;
-uint16_t	minPingTime = 1000;
-uint32_t 	lastHeartBeat = 0;
-uint32_t	EchoLastSend = 0;
-uint32_t	EchoTimeStamp = 0;
-uint32_t	EchoReturn = 0;
-uint32_t 	EchoRuntime = 0;
+volatile	bool 		GotEchoResponse = false;
+volatile	bool 		CheckPingTimeout = false;
+uint32_t				TxFailCounter = 0;
+uint32_t				TxGoodCounter = 0;
+uint16_t				maxPingTime = 1;
+uint16_t				minPingTime = 1000;
+uint32_t 				lastHeartBeat = 0;
+// uint32_t				PingLastSend = 0;
+uint32_t				PingTimeStamp = 0;
+volatile	uint32_t	EchoReturn = 0;
+volatile	uint32_t 	EchoRuntime = 0;
 
 
-void preHwInit()
+void preHwInit() //kein serieller Output 
 {
-	
-	DEBUG_SERIAL(MY_BAUD_RATE);
-	DEBUG_PRINTLN("preHwInit: ");
-	// pinMode(LED_PIN, OUTPUT);   // sets the pin as output
+
 }
 
-void before()
+void before() 
 {
-	DEBUG_PRINTLN("before...");
-}
+	DEBUG_PRINT("Channel / NodeID: ");
+	DEBUG_PRINT(MY_RF24_CHANNEL);
+	DEBUG_PRINT(" / ");
+	DEBUG_PRINTLN(MY_NODE_ID);
 
+}
 
 void presentation()
 {
 	DEBUG_PRINTLN("presentation...");;
 	mySendSketchInfo();
 	myPresentation();
+	// kein eigenes present erforderlich, weil mittels MY_ECHO_NODE über CommonFunctions.h erledigt
 	myHeartBeatLoop();
 }
 
@@ -119,38 +129,39 @@ void setup()
 void loop()
 {
 	uint32_t currentTime = millis();
-	uint32_t TimeSinceEchoSend = currentTime - EchoLastSend;
+	uint32_t TimeSinceLastPing = currentTime - PingTimeStamp;
+	uint32_t TimeoutTime = currentTime - PingTimeStamp;
 	uint32_t TimeSinceHeartBeat = currentTime - lastHeartBeat;
 	
-
+	// DEBUG_PRINTLN("loop");
 	
-	
-	if ((TimeSinceEchoSend > (uint32_t)MAX_ECHO_WAIT) || GotEchoResponse)
+	if (GotEchoResponse)
 	{
 
+		EchoRuntime=currentTime-EchoReturn;
+		send(msgEchoRunTime.set(EchoRuntime));
+		GotEchoResponse=false;
+		CheckPingTimeout=false;
+		TxGoodCounter++;
+		
+		// DEBUG_PRINT("EchoRuntime: ");
+		// DEBUG_PRINT(EchoRuntime);
+		// DEBUG_PRINT(" EchoReturn: ");
+		// DEBUG_PRINT(EchoReturn);
+		// DEBUG_PRINT(" minPing ");
+		// DEBUG_PRINT(minPingTime);
+		// DEBUG_PRINT(" maxPing ");
+		// DEBUG_PRINTLN(maxPingTime);
+		
+		if (EchoRuntime > maxPingTime)
+		{
+			maxPingTime=EchoRuntime;
+		}
+		if (EchoRuntime < minPingTime)
+		{
+			minPingTime=EchoRuntime;
+		}
 
-		if (GotEchoResponse)
-		{
-			wait(20);
-			if (EchoRuntime > 0)
-			{
-				TxGoodCounter++;
-				send(msgEchoRunTime.set(EchoRuntime));
-				if (EchoRuntime > maxPingTime)
-				{
-					maxPingTime=EchoRuntime;
-				}
-				if (EchoRuntime < minPingTime)
-				{
-					minPingTime=EchoRuntime;
-				}
-			}
-		}
-		else
-		{
-			DEBUG_PRINTLN("NOT GotEchoResponse: ");
-			TxFailCounter++;
-		}
 		
 		float PacketRatio=float(float(TxGoodCounter)/float(TxGoodCounter+TxFailCounter)*100.0);
 		send(msgPacketRatio.set(PacketRatio,2));
@@ -173,13 +184,36 @@ void loop()
 		DEBUG_PRINT(minPingTime);
 		DEBUG_PRINT(" maxPing ");
 		DEBUG_PRINTLN(maxPingTime);		
-		
-		EchoLastSend=millis();
-		EchoTimeStamp=EchoLastSend;
-		send(msgEchoTimeStamp.set(EchoTimeStamp));
-		GotEchoResponse=false;
 	}
 	
+	
+	if (TimeSinceLastPing > (uint32_t)PING_INTERVAL)
+	{	
+		// PingLastSend=currentTime;
+		PingTimeStamp=currentTime;
+		send(msgEchoTimeStamp.set(PingTimeStamp));
+		// DEBUG_PRINT("SendPing: ");
+		// DEBUG_PRINTLN(PingTimeStamp);
+		CheckPingTimeout=true;
+		TimeoutTime = 0;	//wird im nächsten Loop berechnet
+	}
+	
+	if (CheckPingTimeout)
+	{
+		// DEBUG_PRINT(F("Check Timeout: "));
+		// DEBUG_PRINTLN(TimeoutTime);
+		if (TimeoutTime > (uint32_t)MAX_ECHO_WAIT)
+		{	
+			// DEBUG_PRINT(F("Timeout G: "));
+			// DEBUG_PRINT(TxGoodCounter);
+			// DEBUG_PRINT(F(" Timeout B: "));
+			// DEBUG_PRINTLN(TxFailCounter);
+			
+			send(msgDebugReturnString.set(F("Timeout")));
+			TxFailCounter++;
+			CheckPingTimeout=false;
+		}
+	}
 	
 	if ((TimeSinceHeartBeat > (uint32_t)HEARTBEAT_INTERVAL))
 	{
@@ -191,44 +225,6 @@ void loop()
 void receive(const MyMessage &message)
 {
 	// DEBUG_PRINTLN("receive");
-	if (!mGetAck(message) && message.sensor == CHILD_ECHO_TIMESTAMP && message.sender == 0)
-	{
-		if (message.type == V_TEXT)
-		{
-			EchoReturn = message.getULong();
-
-			if (EchoReturn == EchoTimeStamp)
-			{
-				EchoRuntime=millis()-EchoTimeStamp;
-				GotEchoResponse=true;
-				EchoTimeStamp=0;
-			}			
-		}
-	}
-	
-	// else
-	// {
-		// DEBUG_PRINTLN("unknown receive");
-		
-		// DEBUG_PRINT("sensor: ");
-		// DEBUG_PRINTLN(message.sensor);
-
-		// DEBUG_PRINT("mGetPayloadType: ");
-		// DEBUG_PRINTLN(mGetPayloadType(message));
-		
-		// DEBUG_PRINT("sender: ");
-		// DEBUG_PRINTLN(message.sender);
-
-		// DEBUG_PRINT("version: ");
-		// DEBUG_PRINTLN(message.version);
-		
-		// DEBUG_PRINT("getByte: ");
-		// uint8_t Temp = message.getByte();
-		// DEBUG_PRINTLN(Temp);
-	// }
-	// DEBUG_PRINTLN("receive");
-	
-	
 	// DEBUG_PRINT("sensor: ");
 	// DEBUG_PRINTLN(message.sensor);
 
@@ -237,30 +233,20 @@ void receive(const MyMessage &message)
 	
 	// DEBUG_PRINT("sender: ");
 	// DEBUG_PRINTLN(message.sender);
+
+	if ((message.sensor == CHILD_ECHO_TIMESTAMP) && (message.sender == 0) && !message.isEcho())
+	{
+		if (message.type == V_TEXT)
+		{
+			EchoReturn = message.getULong();
+
+			if (EchoReturn == PingTimeStamp)
+			{
+				GotEchoResponse=true;
+			}			
+		}
+	}
 		
 }
 
-
-// ....
-// void indication(indication_t ind)
-// {
-  // switch(ind)
-  // {
-    // case INDICATION_TX: 
-		// Serial.println(F("TX"));
-		// LED_Blink(1,1);//Anzahl, Geschwindigkeit
-		// break;
-    // case INDICATION_ERR_FIND_PARENT:          	Serial.println(F("ERR_FIND_PARENT")); break;
-    // case INDICATION_GOT_PARENT:          		Serial.println(F("GOT_PARENT")); break;
-    // case INDICATION_ERR_HW_INIT: 				Serial.println(F("ERR_HW_INIT"));
-		// LED_Blink(10,1);//Anzahl, Geschwindigkeit
-		// break;
-    // case INDICATION_ERR_TX:
-		// Serial.println(F("ERR_TX"));
-		// LED_Blink(1,2);//Anzahl, Geschwindigkeit
-		// break;
-    // case INDICATION_ERR_CHECK_UPLINK:      		Serial.println(F("ERR_CHECK_UPLINK")); break;
-
-  // }
-// }
 
